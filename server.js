bed9307bd8f5e1035c80a124ad902a8fedfea201
Sohan Rout <sohanrout84@gmail.com>
2025-04-12 06:58:54 +0530

    removed news

--- server.js
+++ server.js
@@ -205,30 +205,6 @@ app.post("/api/carbon", async (req, res) => {
   }
 });
 
-// eco-friendly news api
-app.get("/api/eco-news", async (req, res) => {
-  try {
-    const response = await axios.get(
-      "https://api.magicapi.dev/api/v1/spoonacular/world-news-api/search-news",
-      {
-        params: {
-          text: "eco OR environment OR sustainable OR climate OR green",
-          categories: "environment",
-          language: "en",
-        },
-        headers: {
-          "x-api-key": process.env.MAGIC_API_KEY, // your api key here
-        },
-      }
-    );
-
-    res.status(200).json(response.data);
-  } catch (error) {
-    console.error("Eco News API Error:", error.response?.data || error.message);
-    res.status(500).json({ error: "Failed to fetch Eco News" });
-  }
-});
-
 // Start server
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
